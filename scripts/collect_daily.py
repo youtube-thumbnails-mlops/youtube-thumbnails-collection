168bd610c189bd4e314419e978976775768d3e60
Daniele Acquaviva <danieleacquaviva96@gmail.com>
2025-12-29 16:11:18 +0100

    new test

--- scripts/collect_daily.py
+++ scripts/collect_daily.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from youtube_collector import YouTubeClient
 
 # CONSTANTS
-BATCH_LIMIT = 500  # <--- Change this if you want larger batches
+BATCH_LIMIT = 500
 
 def count_samples(metadata_file):
     """Count total samples in metadata CSV."""
