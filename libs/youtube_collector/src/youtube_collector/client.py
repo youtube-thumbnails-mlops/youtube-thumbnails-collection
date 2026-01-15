883fd7d3b2f39984b17d0c9dd9484e0fb6d52975
Daniele Acquaviva <danieleacquaviva96@gmail.com>
2026-01-15 17:43:02 +0100

    refactor: remove 'People & Blogs' class from collector

--- libs/youtube_collector/src/youtube_collector/client.py
+++ libs/youtube_collector/src/youtube_collector/client.py
@@ -31,7 +31,7 @@ class YouTubeClient:
     DEFAULT_CATEGORIES = {
         '1': 'Film & Animation', '2': 'Autos & Vehicles',
         '10': 'Music', '15': 'Pets & Animals', '17': 'Sports', '19': 'Travel & Events',
-        '20': 'Gaming', '22': 'People & Blogs', '23': 'Comedy', '24': 'Entertainment',
+        '20': 'Gaming', '23': 'Comedy', '24': 'Entertainment',
         '25': 'News & Politics', '26': 'Howto & Style', '27': 'Education',
         '28': 'Science & Technology', '29': 'Nonprofits & Activism',
     }
